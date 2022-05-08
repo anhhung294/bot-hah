fd81b6acd0bd8c2e2b78d051db61b376737f2ae1
anhhunglhp2904 <hoanganhhung29042004@gmail.com>
2022-05-08 17:58:42 +0700

    fix host channel

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -3,7 +3,7 @@ const FiveManSquad = require('../features/FiveManSquad.js');
 module.exports = {
 	name: 'voiceStateUpdate',
 	async execute(oldState, newState) {
-		if(newState.channel?.name==='—5-Man Squad—'){
+		if(newState.channel?.id==='971988068984848454'){
             return FiveManSquad(oldState, newState);
         }
 	},
