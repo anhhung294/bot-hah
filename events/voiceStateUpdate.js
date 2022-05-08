7ba299aa8c6152ee00074c0fa31deaad50096409
anhhunglhp2904 <hoanganhhung29042004@gmail.com>
2022-05-08 17:57:23 +0700

    fix name

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -3,7 +3,7 @@ const FiveManSquad = require('../features/FiveManSquad.js');
 module.exports = {
 	name: 'voiceStateUpdate',
 	async execute(oldState, newState) {
-		if(newState.channel?.name){
+		if(newState.channel?.name==='—5-Man Squad—'){
             return FiveManSquad(oldState, newState);
         }
 	},
