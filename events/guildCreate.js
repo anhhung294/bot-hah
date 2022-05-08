341c21f8dd3f6adfa760280f998c4eb287ae647d
anhhunglhp2904 <hoanganhhung29042004@gmail.com>
2022-05-08 18:24:58 +0700

    fix package

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,14 +1,8 @@
 const deployCommandsGuild = require('../deploy-commands-guild.js');
-const gameModel = require('../models/game.js');
 
 module.exports = {
 	name: 'guildCreate',
 	async execute(guild) {
-		const guildId = guild.id;
-		const game = new gameModel({
-			guildId: guild.id
-		});
-		await game.save();
 		deployCommandsGuild(guildId);
 	},			
 };
